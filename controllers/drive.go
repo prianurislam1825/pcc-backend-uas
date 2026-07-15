6f632da0562ec6a685a6a4907f3a9a244312637d
Pria Nur Islam <prianurislam966@gmail.com>
2026-07-16 02:41:19 +0700

    Update Webhook URL to properly configured public deployment

--- controllers/drive.go
+++ controllers/drive.go
@@ -71,7 +71,7 @@ func DriveUpload(c *gin.Context) {
 
 	// Kirim request ke Google Apps Script
 	res, err := http.Post(
-		"https://script.google.com/macros/s/AKfycbznE4U43zLW5u4_VlFLwnWtz_Oj0NyPLCOztCK82vSOnVuftcKSiUvz1b8icUyd12k/exec",
+		"https://script.google.com/macros/s/AKfycby5pRl0-uaG0r-X_zeko0CznaUzlGSfFkYl_ujY0DOBHVbJtzfYRr28qDfa0_jaFL5H/exec",
 		"application/json; charset=UTF-8",
 		requestBody,
 	)
@@ -166,7 +166,7 @@ func DriveUnduh(c *gin.Context) {
 
 	// Request ke Google Apps Script
 	res, err := http.Get(
-		"https://script.google.com/macros/s/AKfycbznE4U43zLW5u4_VlFLwnWtz_Oj0NyPLCOztCK82vSOnVuftcKSiUvz1b8icUyd12k/exec?id=" + id,
+		"https://script.google.com/macros/s/AKfycby5pRl0-uaG0r-X_zeko0CznaUzlGSfFkYl_ujY0DOBHVbJtzfYRr28qDfa0_jaFL5H/exec?id=" + id,
 	)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
